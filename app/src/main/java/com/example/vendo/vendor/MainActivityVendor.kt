8877c74972735520e09f5fe3f2dbc783a20ded10
piush-terminalwitchcraft <piushpaul.16@gmail.com>
2022-03-13 11:45:14 +0530

    Bottom navigation of vendor linked with fragments 🎇

--- app/src/main/java/com/example/vendo/vendor/MainActivityVendor.kt
+++ app/src/main/java/com/example/vendo/vendor/MainActivityVendor.kt
@@ -4,15 +4,24 @@ import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.vendo.R
+import com.example.vendo.vendor.fragments.HomeFragmentVendor
+import com.example.vendo.vendor.fragments.OrganizationFragmentVendor
+import com.example.vendo.vendor.fragments.ProfileFragmentVendor
 import np.com.susanthapa.curved_bottom_navigation.CbnMenuItem
 import java.util.*
 
 class MainActivityVendor : AppCompatActivity() {
 
     private var languageCode : String? = "en"
-
+    private lateinit var navHostFragment: NavHostFragment
     private lateinit var bottomNavigation: np.com.susanthapa.curved_bottom_navigation.CurvedBottomNavigationView
+    private lateinit var navController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         languageCode = getSharedPreferences(getString(R.string.app_name),MODE_PRIVATE)
@@ -21,6 +30,9 @@ class MainActivityVendor : AppCompatActivity() {
         setContentView(R.layout.activity_main_vendor)
 
         bottomNavigation = findViewById(R.id.bottom_nav_vendor)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view_vendor) as NavHostFragment
+        navController = navHostFragment.navController
+
         val menuItems = arrayOf(
             CbnMenuItem(
                 R.drawable.ic_organization,
@@ -38,7 +50,12 @@ class MainActivityVendor : AppCompatActivity() {
 
         bottomNavigation.setMenuItems(menuItems,1)
         bottomNavigation.setOnMenuItemClickListener { cbnMenuItem, i ->
-
+            val destFragment: Fragment = when ( cbnMenuItem.icon){
+                R.drawable.ic_organization-> OrganizationFragmentVendor()
+                R.drawable.ic_profile -> ProfileFragmentVendor()
+                else -> HomeFragmentVendor()
+            }
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container_view_vendor,destFragment).commit()
         }
     }
 
